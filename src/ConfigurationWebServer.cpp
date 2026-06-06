e5b3edf8a44b8abe00693681c5884000056f84a2
Anthony Sturdy <anthonysturdy123@gmail.com>
2026-06-06 21:21:22 +0200

    Config server improvements

--- src/ConfigurationWebServer.cpp
+++ src/ConfigurationWebServer.cpp
@@ -18,16 +18,58 @@ void ConfigurationWebServer::Initialise() {
             String openskyClientSecret = prefs.getString("opensky-secret", "");
             prefs.end();
 
-            String html = "<html><body>";
-            html += "<h1>Configure Micro Radar</h1>";
-            html += "<form action='/save' method='POST'>";
-            html += "<label>Latitude: <input name='latitude' value='" + latitude + "'></label>";
-            html += "<label>Longitude: <input name='longitude' value='" + longitude + "'></label><br>";
-            html += "<label>Radius (in °): <input name='radius' value='" + radius + "'></label><br>";
-            html += "<label>OpenSky Client ID: <input name='opensky-id' value='" + openskyClientId + "'></label><br>";
-            html += "<label>OpenSky Client Secret: <input name='opensky-secret' value='" + openskyClientSecret + "'></label><br>";
-            html += "<input type='submit' value='Save'>";
-            html += "</form></body></html>";
+            std::fill(openskyClientSecret.begin(), openskyClientSecret.end(), '*'); // replace secret string with asterisks
+
+            String html = R"(
+                <html>
+                    <head>
+                        <title>Configure Micro Radar</title>
+                        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4.3.0"></script>
+                    </head>
+                    <body class="font-mono bg-gray-900 text-green-500">
+                        <fieldset class="border border-green-500 p-5 m-10 inline-block">
+                            <legend class="ml-5 px-2">Configure Micro Radar</legend>
+                            <form id="cfg" action='/save' method='POST' class="flex flex-col gap-2">
+                                <div class="flex gap-5">
+                                    <label class="flex items-center gap-2">
+                                        Latitude:
+                                        <input name='latitude' type="number" min="-90" step="0.000001" max="90" value=')" + latitude + R"(' class="border border-green-500">
+                                    </label>
+                                    <label class="flex items-center gap-2">
+                                        Longitude:
+                                        <input name='longitude' type="number" min="-180" step="0.000001" max="180" value=')" + longitude + R"(' class="border border-green-500">
+                                    </label>
+                                </div>
+                                <label class="flex items-center gap-2">
+                                    Radius (in &deg;):
+                                    <input name='radius' type="number" min="0" step="0.000001" max="2" value=')" + radius + R"(' class="flex-1 border border-green-500">
+                                </label>
+                                <label class="flex items-center gap-2">
+                                    OpenSkyAPI Client ID:
+                                    <input name='opensky-id' value=')" + openskyClientId + R"(' class="flex-1 border border-green-500">
+                                </label>
+                                <label class="flex items-center gap-2">
+                                    OpenSkyAPI Client Secret:
+                                    <input name='opensky-secret' value=')" + openskyClientSecret + R"(' class="flex-1 border border-green-500">
+                                </label>
+
+                                <input type='submit' value='Save' class="bg-green-500 text-black mt-4 px-2 self-start cursor-pointer">
+                            </form>
+                        </fieldset>
+
+                        <div id="result" class="px-10"></div>
+
+                        <script>
+                            document.getElementById('cfg').addEventListener('submit', function(e) {
+                                e.preventDefault();
+                                fetch(this.action, { method: 'POST', body: new FormData(this) })
+                                    .then(r => r.text())
+                                    .then(html => document.getElementById('result').innerHTML = html);
+                            });
+                        </script>
+                    </body>
+                </html>
+                )";
 
             request->send(200, "text/html", html);
         }
@@ -47,13 +89,14 @@ void ConfigurationWebServer::Initialise() {
                 prefs.putString("radius", request->getParam("radius", true)->value());
             if (request->hasParam("opensky-id", true))
                 prefs.putString("opensky-id", request->getParam("opensky-id", true)->value());
-            if (request->hasParam("opensky-secret", true))
-                prefs.putString("opensky-secret", request->getParam("opensky-secret", true)->value());
+            if (request->hasParam("opensky-secret", true)) {
+                String secret = request->getParam("opensky-secret", true)->value();
+                if (secret.indexOf('*') == -1) prefs.putString("opensky-secret", secret); // avoid overwriting with obscured string
+            }
             prefs.end();
 
-            request->send(200, "text/html", "<h1>Preferences saved - restarting...</h1>");
+            request->send(200, "text/html", "Saved - restarting device...");
 
-            delay(1000);
             ESP.restart();
         }
     );
